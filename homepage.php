3fd9057ac77d386930c8ffe84cc17d16eb2014f7
1606149 <mebaad10@gmail.com>
2016-10-24 15:21:06 +0100

    new project and session

--- homepage.php
+++ homepage.php
@@ -1,23 +1,20 @@
 <?php
 
 session_start();
-$accesslevel = $_COOKIE['access_level_cookie'];
+$accesslevel = $_SESSION['access_level_cookie'];
 displayAccessLevelInformation($accesslevel);
-
+$_SESSION = array();
+session_destroy();
 function displayAccessLevelInformation($accesslevel)
 {
     if($accesslevel == "standarduser")
     {
         echo "<p style = \"background-color: lightgreen\">You are currently logged in as a standard user</p>";
-        $_SESSION = array();
-        session_destroy();
     }
     elseif($accesslevel == "root")
     {
         echo "<p style = \"background-color: red\">You are currently logged in as a root user</p>";
         echo "<p style = \"background-color: red\">You now have access to additional administrative features</p>";
-        $_SESSION = array();
-        session_destroy();
     }
 }
 
