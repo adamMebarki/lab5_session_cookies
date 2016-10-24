88ff824b3699cf479a5c986ab4919476688ea01d
1606149 <mebaad10@gmail.com>
2016-10-24 15:17:24 +0100

    new project and session

--- checkLogin.php
+++ checkLogin.php
@@ -1,14 +1,16 @@
 <?php
-    $username = $_POST["name"];
-    $password = $_POST["password"];
-
-    if ($username == "username" && $password == "password"){
-        setcookie('access_level_cookie','standarduser');
+    //$username = $_POST["name"];
+    //$password = $_POST["password"];
+    $_SESSION['username'] = $_POST["name"];
+    $_SESSION['password'] = $_POST["password"];
+    if ($_SESSION['username'] == "username" && $_SESSION['password'] == "password"){
+        //setcookie('access_level_cookie','standarduser');
+        $_SESSION['access_level_session'] = "standarduser";
     }
     elseif($username == "root" && $password == "root")
     {
-        setcookie('access_level_cookie','root');
-
+        //setcookie('access_level_cookie','root');
+        $_SESSION['access_level_session'] = "root";
     }
 header('Location: homepage.php');
 ?>
