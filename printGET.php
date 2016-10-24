c19305fef4bb128127ccc7426eae11cf5973deec
1606149 <mebaad10@gmail.com>
2016-10-24 16:34:55 +0100

    new project and session

--- printGET.php
+++ printGET.php
@@ -6,17 +6,6 @@
  * Time: 15:57
  */
 
-// Récupération des variables GET
-$tabUrl = parse_url ( $_SERVER [ 'REQUEST_URI' ] ) ;
-$listparam = explode ( "&amp;" , $tabUrl [ 'query' ] ) ;
-$nb_param = count ( $listparam ) ;
-
-// on associe les valeurs
-for ( $i=0 ; $i<$nb_param ; $i++)  {
-    $param = explode ( '=' , $listparam[$i] ) ;
-    $paramname = $param[0];
-    $paramvalue = $param[1];
-    $$paramname = $paramvalue;
-    echo $paramname . " : " . $paramvalue;
-}
+    $test = $_GET["query"];
+    echo $test
 ?>
